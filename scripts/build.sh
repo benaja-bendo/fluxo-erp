cc922280b8ab7da44df272e4b7abdf212e4fc93b
benaja-bendo <benaja.bendo02@gmail.com>
2025-04-15 11:40:52 +0200

    modification et ajout: - changement de dossiers pour icon.icns - ajout d'un icon.ico pour la génération pour win

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,7 +15,7 @@ jpackage \
   --main-jar Fluxo-0.0.1-SNAPSHOT.jar \
   --type dmg \
   --dest installer/ \
-  --icon src/main/resources/icon.icns \
+  --icon src/main/resources/assets/icones/icon.icns \
   --module-path "$PATH_TO_FX_MODS" \
   --add-modules javafx.controls,javafx.fxml \
   --java-options "--add-opens java.base/java.lang=ALL-UNNAMED" \
